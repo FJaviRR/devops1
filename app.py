9918b1c16a5774bb0123a1fc3342b5b4dc6dee99
FJaviRodriguez <fjrodriguezruiz1708@gmail.com>
2026-04-27 18:30:45 +0200

    pylint a app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-""" Docstring del módulo """
+"""Módulo para realizar operaciones aritméticas básicas."""
 
 
-    def suma(a: int, b: int) -> int:
-        """ docstring function
-        # debe dar errores en linter
-        """
-        x = "no se usa."
-        return a+ b
+def suma(a_val: int, b_val: int) -> int:
+    """
+    Calcula la suma de dos números enteros.
+
+    Args:
+        a_val (int): El primer número.
+        b_val (int): El segundo número.
+
+    Returns:
+        int: La suma de a y b.
+    """
+    return a_val + b_val
